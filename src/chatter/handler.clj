1bd01698f93659c024d8a1c7557130ffac96e542
Laura Abend <laura.l.abend@gmail.com>
2017-12-12 17:14:48 -0600

    add heroku deployment info

--- src/chatter/handler.clj
+++ src/chatter/handler.clj
@@ -1,10 +1,14 @@
 (ns chatter.handler
+  (:gen-class)
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [ring.middleware.params :refer [wrap-params]]
+            [ring.adapter.jetty :as jetty]
             [hiccup.page :as page]
-            [hiccup.form :as form]))
+            [hiccup.form :as form]
+            [ring.util.anti-forgery :as anti-forgery]
+            [environ.core :refer [env]]))
 
 (def chat-messages (atom '()))
 
@@ -13,7 +17,10 @@
   [messages]
   (page/html5
    [:head
-    [:title "chatter"]]
+    [:title "chatter"]
+    (page/include-css "//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css")
+    (page/include-js  "//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js")
+    (page/include-css "/chatter.css")]
    [:body
     [:h1 "Our Chat App"]
     [:p
@@ -23,7 +30,7 @@
       "Message: " (form/text-field "msg")
       (form/submit-button "Submit"))]
     [:p
-     [:table
+     [:table#messages.table.table-bordered.table-hover
       (map (fn [m] [:tr [:td (:name m)] [:td (:message m)]]) messages)]]]))
 
 (defn update-messages!
@@ -39,6 +46,18 @@
           new-messages (update-messages! chat-messages name-param msg-param)]
       (generate-message-view new-messages)
       ))
+  (route/resources "/")
   (route/not-found "Not Found"))
 
 (def app (wrap-params app-routes))
+
+(defn init []
+  (println "chatter is starting"))
+
+
+(defn destroy []
+  (println "chatter is shutting down"))
+
+  (defn -main [& [port]]
+  (let [port (Integer. (or port (env :port) 5000))]
+    (jetty/run-jetty #'app {:port port :join? false})))
